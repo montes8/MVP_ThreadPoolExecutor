873963f415f67afc550e1e5b43ed31ccaa67741c
montes8 <emontesinos812@gmail.com>
2019-01-05 09:20:06 -0500

    implementando interfas LoginView en el activity

--- app/src/main/java/com/example/tayler_gabbi/mvp_threadpoolexecutor/LoginActivity.java
+++ app/src/main/java/com/example/tayler_gabbi/mvp_threadpoolexecutor/LoginActivity.java
@@ -3,14 +3,26 @@ package com.example.tayler_gabbi.mvp_threadpoolexecutor;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.example.tayler_gabbi.mvp_threadpoolexecutor.view.LoginView;
+
+public class LoginActivity extends AppCompatActivity implements LoginView{
+
+    private EditText editPass,editUser;
+    private Button logearse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        editUser = findViewById(R.id.edit_usuario);
+        editPass = findViewById(R.id.edit_pasword);
+        logearse = findViewById(R.id.button_ingresar);
 
      /*   DefaultExecutorSupplier.getInstance().forBackgroundTasks().execute(new Runnable() {
 
@@ -37,5 +49,25 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 });*/
+    }
+
+    @Override
+    public void loginValidations() {
+
+        Toast.makeText(LoginActivity.this, "Debes ingresar campos correspondientes", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void loginSuccess() {
+        Intent intent = new Intent(LoginActivity.this,HomeActivity.class);
+        startActivity(intent);
+        editUser.setText("");
+        editPass.setText("");
+        finish();
+    }
+
+    @Override
+    public void loginError() {
+        Toast.makeText(LoginActivity.this, "Usuario o Contraseña Incorrectos", Toast.LENGTH_LONG).show();
     }
 }

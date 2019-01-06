0d45e635007365402e33a20bbe9152528ca314a2
montes8 <emontesinos812@gmail.com>
2019-01-06 10:02:19 -0500

    creando interfaz pata mvp de regitro de usuario

--- app/src/main/java/com/example/tayler_gabbi/mvp_threadpoolexecutor/RegisterActivity.java
+++ app/src/main/java/com/example/tayler_gabbi/mvp_threadpoolexecutor/RegisterActivity.java
@@ -1,13 +1,48 @@
 package com.example.tayler_gabbi.mvp_threadpoolexecutor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.example.tayler_gabbi.mvp_threadpoolexecutor.view.RegisterView;
+
+public class RegisterActivity extends AppCompatActivity implements RegisterView{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    @Override
+    public void registrarSuccess() {
+        Intent intent = new Intent(this,HomeActivity.class);
+        startActivity(intent);
+        Toast.makeText(this,"usuario registrado",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void nameError() {
+
+        Toast.makeText(this,"Ingrese nombre",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void usuarioError() {
+
+        Toast.makeText(this,"Ingrese nombre de usuario",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void passwordError() {
+
+        Toast.makeText(this,"Ingrese  Contraseña",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void registerError() {
+
+        Toast.makeText(this,"usuario no guardado", Toast.LENGTH_LONG).show();
     }
 }
---
+++ app/src/main/java/com/example/tayler_gabbi/mvp_threadpoolexecutor/view/RegisterView.java
@@ -0,0 +1,16 @@
+package com.example.tayler_gabbi.mvp_threadpoolexecutor.view;
+
+public interface RegisterView {
+
+
+    void registrarSuccess();
+
+    void nameError();
+
+    void usuarioError();
+
+    void passwordError();
+
+    void registerError();
+
+}
